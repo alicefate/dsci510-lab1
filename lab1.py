b80528230316bed4a9fe206cc56431fa28f2c7c9
Kexin Yan <33647003+alicefate@users.noreply.github.com>
2020-08-29 02:04:50 +0800

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
-# I realize if this is not a function, I can't invoke it. I can only write it again.
+# Welcome others to class
 def has_to_be_a_function_to_be_invoked():
     print('Welcome to DCSCI510!')
 
